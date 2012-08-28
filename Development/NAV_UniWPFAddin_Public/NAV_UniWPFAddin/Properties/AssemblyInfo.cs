374d34da74b1c4087af489bcfa9b3c9d0258ae54
Kamil Sacek <kamil.sacek@navertica.com>
2012-08-28 04:54:04 +0000

    Initial public version

---
+++ Development/NAV_UniWPFAddin_Public/NAV_UniWPFAddin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NAV_UniWPFAddin_Public")]
+[assembly: AssemblyDescription("Universal WPF NAV Addin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kamil Sacek, NAVERTICA a.s.")]
+[assembly: AssemblyProduct("NAV_UniWPFAddin_Public")]
+[assembly: AssemblyCopyright("Copyright © NAVERTICA a.s. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4D0B6207-47FA-4208-AD09-DBAD094F135E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
